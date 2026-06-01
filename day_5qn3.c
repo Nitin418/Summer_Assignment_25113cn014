665cd08a708ec94afb569c22ce51484b23e262b8
Nitin418 <pal568354@gmail.com>
2026-06-01 18:36:13 +0530

    DAY-5 uploaded

---
+++ day_5qn3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i,num;
+    printf("enter the integer number:");
+    scanf("%d",&num);
+    printf("factors of %d are:\n",num);
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
