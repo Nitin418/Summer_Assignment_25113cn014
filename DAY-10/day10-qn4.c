bace52a69900037fe3772c75505d1d97083c27f5
Nitin418 <pal568354@gmail.com>
2026-06-07 22:45:53 +0530

    DAY-10 uploaded

---
+++ DAY-10/day10-qn4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5-i;j++)
+        {
+            printf("");
+        }
+        for(int j=1;j<=i;j++){
+            printf("%c",j+64);
+        }
+        for(int k=i-1;k>=1;k--)
+        {
+            printf("%c",k+64);
+        }
+        printf("\n");
+    }
+    return 0;
+}
